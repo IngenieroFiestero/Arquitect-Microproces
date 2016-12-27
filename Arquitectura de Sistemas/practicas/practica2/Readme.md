7c4f40b0bec8fe894276be76a48131b9f69c2c0d
samuel <ingenierofiestero@gmail.com>
2016-12-27 12:39:33 +0100

    Practica 2

---
+++ Arquitectura de Sistemas/practicas/practica2/Readme.md
@@ -0,0 +1,21 @@
+Ejercicio. Escribe un programa en C que vuelque por pantalla el contenido de la variable argv. Además  debe  contabilizar  el  número  de  palabras  y  caracteres  que  contiene  dicha  línea  de comandos. Por ejemplo: 
+
+```
+$mipc> ./miprog a b c 0.1 raimundo 
+```
+
+miprog 
+
+a
+
+b
+
+c 
+
+0.1 
+
+raimundo 
+
+Número de palabras: 6 
+
+Número de caracteres: 25
---
+++ Arquitectura de Sistemas/practicas/practica2/pr2_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+//gcc -o miprog pr2_1.c 
+//./miprog a b c 0.1 raimundo 
+int main(int argc,char *argv[]){
+    //Eliminamos el ./
+    int caracteres = (sizeof argv[0])-2;
+    printf("%.*s\n", caracteres, argv[0]+ 2);
+    for(int i=1; i < argc;i++){
+        printf("%s\n",argv[i]);
+        caracteres += strlen(argv[i]) + 1;//Caracter extra del espacio
+    }
+    printf("Numero de palabras: %d\nNumero de caracteres: %d\n",argc,caracteres);
+	//El /n indica final de linea y vacia el buffer.
+}
